56f3f537a72fd3bf60c25e46a1787b95d01e5ba1
Hannah Malm <hannahemalm616@gmail.com>
2021-09-20 20:46:55 -0400

    called CLI env

---
+++ lib/groupon/cli.rb
@@ -0,0 +1,10 @@
+class Groupon::CLI 
+    #Use this namespacing because a user might have another file called CLI so this prevents confusion
+
+    def call 
+    puts "Welcome to the Groupon filder!"
+    
+    end 
+
+
+end 
