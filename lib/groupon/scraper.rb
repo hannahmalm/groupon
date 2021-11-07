1ce5c3dd2633e65df56743e36b6b714a7d6ea452
Hannah Malm <hannahemalm616@gmail.com>
2021-11-07 17:21:05 -0500

    added in new attribute scrapes

--- lib/groupon/scraper.rb
+++ lib/groupon/scraper.rb
@@ -5,8 +5,17 @@ require 'byebug'
 class Groupon::Scraper 
     def self.scrape_offers 
         doc = Nokogiri::HTML(open("https://www.groupon.com/local"))
-
-        offers = doc.css(".grpn-dc-title")
+        array_of_offers = doc.css(".deal-card").css('a')
+        array_of_offers.each do |offer_list|
+            attributes = {
+                 title: offer_list.css('.grpn-dc-title')[0].children.text,
+                 url: offer_list.attribute('href').value,
+                 location: offer_list.css('.grpn-dc-loci').text,
+                 price: offer_list.css('.wh-dc-price-discount').text,
+                 rating: offer_list.css('.grpn-rating').text
+             }
+             offer = Groupon::Offer.new(attributes)
+         end 
 
         #as i iterate over the offers, it will print out the offer titles 
         offers.each do |t|
