2b85f7383710e898bbc28b02a33699aa3d7608da
Hannah Malm <hannahemalm616@gmail.com>
2021-11-07 17:20:13 -0500

    switched html website

--- lib/groupon/scraper.rb
+++ lib/groupon/scraper.rb
@@ -4,7 +4,7 @@ require 'pry'
 require 'byebug'
 class Groupon::Scraper 
     def self.scrape_offers 
-        doc = Nokogiri::HTML(open("https://www.groupon.com/local/things-to-do"))
+        doc = Nokogiri::HTML(open("https://www.groupon.com/local"))
 
         offers = doc.css(".grpn-dc-title")
 
@@ -37,41 +37,6 @@ def self.scrape_detail(offer)
 end 
 
 end 
-    # def get_details 
-    #     self.get_page.css(".grpn")
-    # end 
 
-    # def make_details 
-    #     self.get_details.each do |offer|
-    #         # detail = Groupon::Detail.new
-    #         # detail.location = offer.css(".grpn-dc-loci").text
-    #         location = offer.css(".grpn-dc-loci").text
-    #         Groupon::Detail.new(location)
-    #     end 
-    # end 
-
-    # def print_details
-    #     self.make_details 
-    #     Groupon::Detail.all.each do |offer|
-    #         puts "Location: #{offer.location}"
-    #     end 
-    # end 
-
-
-    # def location
-    #     doc = Nokogiri::HTML(open("https://www.groupon.com/local/things-to-do"))
-    #     offers = doc.css(".grpn-dc-loci").first
-    #     offers.each do |t|
-    #         location = t.text 
-    #         Groupon::Detail.new(location)
-    #     end 
-    # end 
-
-    # def print_location 
-    #     self.scrape_location
-    #     Groupon::Detail.all.each do |offer|
-    #     puts "Location: #{offer.location}"
-    #     end 
-    # end 
 
 
